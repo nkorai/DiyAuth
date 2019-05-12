f99bec46cea4806ed400bf52f5aae2f9e8c21632
Naush <nkorai@ricssoftware.com>
2019-05-12 16:44:32 -0400

    Refactoring and adding backend selection page to the sample app

--- DiyAuth/SampleApp/SampleApp/App.xaml.cs
+++ DiyAuth/SampleApp/SampleApp/App.xaml.cs
@@ -82,7 +82,7 @@ namespace SampleApp
 					// When the navigation stack isn't restored navigate to the first page,
 					// configuring the new page by passing required information as a navigation
 					// parameter
-					rootFrame.Navigate(typeof(MainPage), e.Arguments);
+					rootFrame.Navigate(typeof(BackendProviderPage), e.Arguments);
 				}
 				// Ensure the current window is active
 				Window.Current.Activate();
--- DiyAuth/SampleApp/SampleApp/AzureConfigurationPage.xaml.cs
+++ DiyAuth/SampleApp/SampleApp/AzureConfigurationPage.xaml.cs
@@ -8,9 +8,9 @@ namespace SampleApp
 	/// <summary>
 	/// An empty page that can be used on its own or navigated to within a Frame.
 	/// </summary>
-	public sealed partial class MainPage : Page
+	public sealed partial class AzureConfigurationPage : Page
 	{
-		public MainPage()
+		public AzureConfigurationPage()
 		{
 			this.InitializeComponent();
 
